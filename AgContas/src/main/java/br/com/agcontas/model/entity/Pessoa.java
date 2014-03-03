946139d69851da4e5a4f2f4be9a1f514b5cc9b90
roberto <roberto@localhost>
2014-03-03 15:41:50 -0300

    Adicionado Entidade Pessoa

---
+++ AgContas/src/main/java/br/com/agcontas/model/entity/Pessoa.java
@@ -0,0 +1,144 @@
+package br.com.agcontas.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author roberto
+ */
+@Entity
+@Table(name = "Pessoa")
+
+public class Pessoa implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    
+    @Column(name="id", nullable=false)    
+    private Integer idPessoa;
+    
+    @Column(name="tipopessoa_id",length = 1, nullable = false)
+    private Integer TipoPessoa;
+    
+    @Column(name="cnpjcpf", length = 14, nullable = false, unique = true)
+    private String CNPJCPF;
+    
+    @Column(name="nomerazao", length= 100, nullable = false)
+    private String NomeRazao;
+    
+    @Column(name="nomefantasia", length= 100, nullable = false)
+    private String NomeFantasia;
+    
+    @Column(name="ativoouinativo",length = 1, nullable = false)
+    private String AtivoOuInativo;
+   
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date DataCriacao;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date DataAlteracao;
+
+    public Pessoa() {
+        
+    }
+
+    public Integer getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public Integer getTipoPessoa() {
+        return TipoPessoa;
+    }
+
+    public void setTipoPessoa(Integer TipoPessoa) {
+        this.TipoPessoa = TipoPessoa;
+    }
+
+    public String getCNPJCPF() {
+        return CNPJCPF;
+    }
+
+    public void setCNPJCPF(String CNPJCPF) {
+        this.CNPJCPF = CNPJCPF;
+    }
+
+    public String getNomeRazao() {
+        return NomeRazao;
+    }
+
+    public void setNomeRazao(String NomeRazao) {
+        this.NomeRazao = NomeRazao;
+    }
+
+    public String getNomeFantasia() {
+        return NomeFantasia;
+    }
+
+    public void setNomeFantasia(String NomeFantasia) {
+        this.NomeFantasia = NomeFantasia;
+    }
+
+    public String getAtivoOuInativo() {
+        return AtivoOuInativo;
+    }
+
+    public void setAtivoOuInativo(String AtivoOuInativo) {
+        this.AtivoOuInativo = AtivoOuInativo;
+    }
+
+    public Date getDataCriacao() {
+        return DataCriacao;
+    }
+
+    public void setDataCriacao(Date DataCriacao) {
+        this.DataCriacao = DataCriacao;
+    }
+
+    public Date getDataAlteracao() {
+        return DataAlteracao;
+    }
+
+    public void setDataAlteracao(Date DataAlteracao) {
+        this.DataAlteracao = DataAlteracao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.idPessoa);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.idPessoa, other.idPessoa)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    
+}
